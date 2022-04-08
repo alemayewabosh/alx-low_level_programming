ec2d5966c84f8d3442e75a3e8d9e20d275955bfa
Alemayew Abate <alemayew.abonesh07@gmail.com>
2022-04-09 00:42:46 +0300

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,39 +1,33 @@
 #include <stdio.h>
 /**
-* main - prints all possible different combinations of two digits.
-* Return: Always 0 (Success)
+* main - prints all possible combinations of two two-digit numbers,
+* 	 ranging from 0-99, separated by a comma followed by a space.
+*
+* Return: Always 0.
 */
 int main(void)
 {
-int ones = '0';
-int tens = '0';
-int tens2 = '0';
-int ones2 = '0';
-for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+int num1, num2;
+
+for (num1 = 0; num1 <= 98; num1++)
 {
-for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-for (tens2 = '0'; tens2 <= '9'; tens2++)/* prints tens digit*/
-{
-for (ones2 = '0'; ones2 <= '9'; ones2++)/* prints ones digit*/
-{
-if ((ones < ones2) && (tens <= tens2))
-{
-putchar(tens);
-putchar(ones);
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
-putchar(tens2);
-putchar(ones2);
-if (!(ones == '8' && tens == '9'))/*addes comma and space*/
-{
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+continue;
+
 putchar(',');
 putchar(' ');
 }
 }
-}
-}
-}
-}
+
 putchar('\n');
+
 return (0);
 }
