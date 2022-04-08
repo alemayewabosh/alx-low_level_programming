e97e48f82698a726f0594ebe6fb2961fa462dd7c
Alemayew Abate <alemayew.abonesh07@gmail.com>
2022-04-08 05:10:09 +0300

    Task008

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <unistd.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
-*
-*description comment
-*/
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
