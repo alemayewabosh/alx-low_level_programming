c90e059d2cdeef99b230224736bb6c3ee968323e
Alemayew Abate <alemayew.abonesh07@gmail.com>
2022-04-12 23:39:35 +0300

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * main - random numbers
 * Return: last digit
 */
 int main(void)
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
 _putchar('t');
-_putchar('o');
-_putchar('n');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
